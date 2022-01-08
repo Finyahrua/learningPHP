688f9b8a293eac55155ea1eb233bec9432682d0a
Finyahrua <finaddy@gmail.com>
2022-01-08 15:47:54 +0300

    created the project footer

---
+++ Project/templates/footer.php
@@ -0,0 +1,17 @@
+
+<!-- Footer -->
+<footer >
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 danger" style="left:40%;margin-top:20px;margin-bottom:20px">© 2021 Copyright:
+        <a href=""> Finyah_Krukas.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
