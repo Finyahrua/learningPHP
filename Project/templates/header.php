c41f3d2cb05ea489291461d6467a2c32da37a907
Finyahrua <finaddy@gmail.com>
2022-01-08 15:47:11 +0300

    created the project header

---
+++ Project/templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Events Management System </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&family=Racing+Sans+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&family=Mulish:wght@200&family=Oxygen:wght@300&family=Racing+Sans+One&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body style="background-color:#F7F8FC">
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid" style="margin-right:90px;margin-left:90px;">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#" style="font-size:40px;font-weight:400;color:white;font-family: 'Racing Sans One', cursive">EvM</a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#" style="font-size:20px;color:white;font-family: 'Oxygen', sans-serif;
+                font-weight:700"> HOME</a></li>
+                <li><a href="#" style="font-size:20px;color:white;font-family: 'Oxygen', sans-serif;
+                font-weight:700""> ABOUT</a></li>
+            </ul>
+        </div>
+    </nav>
