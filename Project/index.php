8576e80d5a296a2c9267842980451107261b4ab9
Finyahrua <finaddy@gmail.com>
2022-01-08 15:48:56 +0300

    This is how the home screen of our UI would look like

---
+++ Project/index.php
@@ -0,0 +1,156 @@
+<?php include('./templates/header.php') ?>
+<div class="container">
+    <!-- begin of events and add events navbar -->
+    <div style="
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    ">
+        <div>
+            <h3 style="font-size: 50px;font-family: 'Oxygen', sans-serif;">Events</h3>
+        </div>
+        <div>
+            <button type="button" class="btn btn-lg btn-success" style="font-family: 'Oxygen', sans-serif;font-size:25px;font-weight:700">Add Avent</button>
+
+        </div>
+    </div>
+    <hr style=" border: 1px solid #AAAAAA;margin-top:-3px" />
+    <!-- end of events and add events navbar -->
+
+    <!-- begin of table details headers-->
+    <div style="
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin:-5px 155px -15px 25px;font-family: 'Mulish', sans-serif;color:#9C9FB2;
+    ">
+
+        <div>
+            <h5 style="font-weight:700;font-size:20px;">Event Name</h5>
+        </div>
+        <div style="
+    display: flex;
+    justify-content: space-around;
+    align-items: center;">
+            <div style="margin-right:120px">
+                <h3 style="font-weight:700;font-size:20px;">Creator Name</h3>
+            </div>
+            <div>
+                <h3 style="font-weight:700;font-size:20px;">Event Date</h3>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- end of table details headers-->
+    <!-- begin of table details cards -->
+    <div style="border: 1px solid #E7E7EA;align-items: center;font-family: 'Mulish', sans-serif;background-color:#FBF8F8;margin-top:20px">
+        <div style="display: flex;justify-content: space-between;">
+            <div style="margin-left:25px">
+                <h5 style="font-weight:700;font-size:20px;">My Birthday</h5>
+                <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">Created on : 20/12/2021</h5>
+            </div>
+            <div style="display: flex;justify-content: space-around;margin-right:150px">
+                <div style="margin-right:157px">
+                    <h5 style="font-weight:700;font-size:20px;">John Doe</h5>
+                </div>
+                <div>
+                    <h5 style="font-weight:700;font-size:20px;">23/02/2016</h5>
+                    <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">23 45 hrs</h5>
+                </div>
+                <div style="margin-right:-110px">
+                    <button type="button" class="btn  btn-info" style="font-family: 'Oxygen', sans-serif;font-size:20px;font-weight:700; margin:12px -20px 0px 42px">Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- begin of table details cards -->
+    <div style="border: 1px solid #E7E7EA;align-items: center;font-family: 'Mulish', sans-serif;background-color:#FBF8F8;margin-top:20px">
+        <div style="display: flex;justify-content: space-between;">
+            <div style="margin-left:25px">
+                <h5 style="font-weight:700;font-size:20px;">My Birthday</h5>
+                <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">Created on : 20/12/2021</h5>
+            </div>
+            <div style="display: flex;justify-content: space-around;margin-right:150px">
+                <div style="margin-right:157px">
+                    <h5 style="font-weight:700;font-size:20px;">John Doe</h5>
+                </div>
+                <div>
+                    <h5 style="font-weight:700;font-size:20px;">23/02/2016</h5>
+                    <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">23 45 hrs</h5>
+                </div>
+                <div style="margin-right:-110px">
+                    <button type="button" class="btn  btn-info" style="font-family: 'Oxygen', sans-serif;font-size:20px;font-weight:700; margin:12px -20px 0px 42px">Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="border: 1px solid #E7E7EA;align-items: center;font-family: 'Mulish', sans-serif;background-color:#FBF8F8;margin-top:20px">
+        <div style="display: flex;justify-content: space-between;">
+            <div style="margin-left:25px">
+                <h5 style="font-weight:700;font-size:20px;">My Birthday</h5>
+                <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">Created on : 20/12/2021</h5>
+            </div>
+            <div style="display: flex;justify-content: space-around;margin-right:150px">
+                <div style="margin-right:157px">
+                    <h5 style="font-weight:700;font-size:20px;">John Doe</h5>
+                </div>
+                <div>
+                    <h5 style="font-weight:700;font-size:20px;">23/02/2016</h5>
+                    <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">23 45 hrs</h5>
+                </div>
+                <div style="margin-right:-110px">
+                    <button type="button" class="btn  btn-info" style="font-family: 'Oxygen', sans-serif;font-size:20px;font-weight:700; margin:12px -20px 0px 42px">Details</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div style="border: 1px solid #E7E7EA;align-items: center;font-family: 'Mulish', sans-serif;background-color:#FBF8F8;margin-top:20px">
+        <div style="display: flex;justify-content: space-between;">
+            <div style="margin-left:25px">
+                <h5 style="font-weight:700;font-size:20px;">My Birthday</h5>
+                <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">Created on : 20/12/2021</h5>
+            </div>
+            <div style="display: flex;justify-content: space-around;margin-right:150px">
+                <div style="margin-right:157px">
+                    <h5 style="font-weight:700;font-size:20px;">John Doe</h5>
+                </div>
+                <div>
+                    <h5 style="font-weight:700;font-size:20px;">23/02/2016</h5>
+                    <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">23 45 hrs</h5>
+                </div>
+                <div style="margin-right:-110px">
+                    <button type="button" class="btn  btn-info" style="font-family: 'Oxygen', sans-serif;font-size:20px;font-weight:700; margin:12px -20px 0px 42px">Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="border: 1px solid #E7E7EA;align-items: center;font-family: 'Mulish', sans-serif;background-color:#FBF8F8;margin-top:20px">
+        <div style="display: flex;justify-content: space-between;">
+            <div style="margin-left:25px">
+                <h5 style="font-weight:700;font-size:20px;">My Birthday</h5>
+                <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">Created on : 20/12/2021</h5>
+            </div>
+            <div style="display: flex;justify-content: space-around;margin-right:150px">
+                <div style="margin-right:157px">
+                    <h5 style="font-weight:700;font-size:20px;">John Doe</h5>
+                </div>
+                <div>
+                    <h5 style="font-weight:700;font-size:20px;">23/02/2016</h5>
+                    <h5 style="font-weight:700;font-size:15px;color:#9C9FB2">23 45 hrs</h5>
+                </div>
+                <div style="margin-right:-110px">
+                    <button type="button" class="btn  btn-info" style="font-family: 'Oxygen', sans-serif;font-size:20px;font-weight:700; margin:12px -20px 0px 42px">Details</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+</div>
+</div>
+
+
+<?php include('./templates/footer.php') ?>
